c9510e44a174839a24efa516592b591a49a8ecaa
AnastasijaD1999 <anastasijadimitrijevic099@gmail.com>
2021-12-23 23:43:19 +0100

    Migracije za kreiranje tabela u bazi podataka

---
+++ database/migrations/2021_12_23_223847_create_klijents_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKlijentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('klijents', function (Blueprint $table) {
+            $table->id();
+            $table->string('ime');
+            $table->string('prezime');
+            $table->string('email');
+            $table->string('telefon');
+            $table->foreignId('advokat_id')->constrained('advokats');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('klijents');
+    }
+}
