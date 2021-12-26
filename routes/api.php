341881edc127582c1be2c3b2b8fb40311544b3a7
AnastasijaD1999 <anastasijadimitrijevic099@gmail.com>
2021-12-26 14:58:58 +0100

    AuthController, korisnik mora da bude autentifikovan da bi pristupio podacima preko REST API-a

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\API\AdvokatController;
 use App\Http\Controllers\API\AdvokatskaKancelarijaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,10 +17,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('advokatska_kancelarija', AdvokatskaKancelarijaController::class);
-Route::resource('advokat', AdvokatController::class);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::resource('advokatska_kancelarija', AdvokatskaKancelarijaController::class);
+    Route::resource('advokat', AdvokatController::class);
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
